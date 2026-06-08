c146eb0d8153b540a63cdb82afd4bea7d9694da0
Otter <scootutter@gmail.com>
2026-06-08 13:31:14 +0700

    link 2.

--- SV_UniqueCards/Core.cs
+++ SV_UniqueCards/Core.cs
@@ -75,6 +75,7 @@ namespace SV_UniqueCards
             // https://docs.unity3d.com/ScriptReference/ImageConversion.LoadImage.html
             // https://docs.unity3d.com/ScriptReference/Texture2D.html
             // https://docs.unity3d.com/ScriptReference/Sprite.Create.html
+            // https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/basic-writing-and-formatting-syntax
         }
 
     }
